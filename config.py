57252e022cd4b88cfa3932bd1192b7253e559480
Mitchell-Johnson <mitchgj@hotmail.com>
2021-07-19 21:05:51 -0700

    this is gonna be awesome

---
+++ config.py
@@ -0,0 +1,7 @@
+API_KEY = 'PKBKGUIQ0WJ6RMF2IIJ2'
+SECRET_KEY = 'xavXrFtnn0f9vuEtji8jQ3kKsmedkCVshP20qfIp'
+BASE_URL = 'https://paper-api.alpaca.markets'
+DATA_URL = 'https://data.alpaca.markets'
+ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
+ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
